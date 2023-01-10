2c8ca0c1ff1db7963d88af8b4549ee4c701ffb3f
fernando-collab <zaspido@hotmail.com>
2023-01-10 18:11:48 +0100

    codigo base

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <AS5600.h>
+
+AS5600  encoder;
+float output;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // get the angle in degrees of the encoder
+  output = encoder.getAngle();
+  Serial.println(output);
 }
+
